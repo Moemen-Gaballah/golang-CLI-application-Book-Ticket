ba5d13e717bfc50af070e5b2ab40d512e77ed78e
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-02-28 20:11:02 +0200

    fix issue total ticket less that ticket entered number for booking

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -29,14 +32,30 @@ func main() {
 
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
-
+		if userTickets > remainingTickets {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
+			continue
+		}
 		remainingTickets = remainingTickets - userTickets
 		bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at ", firstName, lastName, userTickets)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
 
-		fmt.Printf("These are all out bookings: %v\n", bookings)
+		firstNames := []string{}
+
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("These are all out bookings: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Out conference is booked out. Come back next year.")
+			break
+		}
 
 	}
 }
