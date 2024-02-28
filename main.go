03e3a9a422d3c75f51bbeeb52b45dc12f1aeb1ec
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-02-28 19:12:19 +0200

    init first booking ticket system cli

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
 package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at ", firstName, lastName, userTickets)
+		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+		fmt.Printf("These are all out bookings: %v\n", bookings)
+
+	}
+}
